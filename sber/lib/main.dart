af06dff36cf4ff1eb712b774cb5b7231ca7bf768
ilvas <il.vsl0110@gmail.com>
2020-10-16 18:47:51 +0300

    GodBranch

--- sber/lib/main.dart
+++ sber/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
+//fuck
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
