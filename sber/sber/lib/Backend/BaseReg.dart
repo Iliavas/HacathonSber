ca01540977360f96bc4b092d77332bd29147d5f8
ilvas <il.vsl0110@gmail.com>
2020-10-17 09:27:18 +0300

    fix

---
+++ sber/sber/lib/Backend/BaseReg.dart
@@ -0,0 +1,10 @@
+import 'PersonTypesEnum.dart';
+
+class BasePerson {
+  String name, surname, middlename, email;
+
+  int lvl=1;
+
+  Role role;
+}
+//asdfasd
---
+++ sber/sber/lib/Backend/PersonTypesEnum.dart
@@ -0,0 +1,6 @@
+enum Role {
+  children,
+  teacher,
+  expert,
+  drawer
+}
