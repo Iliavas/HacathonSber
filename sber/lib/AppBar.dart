d72a4a61754010922f7ab275e6fc285b3fbb6821
firesieht <firesieht@mail.ru>
2020-10-17 13:19:18 +0300

    Сделан app bar и стандартное приложение (закоменчено и находится в menu.dart), теперь вы можеете на изи все делать чист

--- sber/lib/AppBar.dart
+++ sber/lib/AppBar.dart
@@ -1,25 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'menu.dart';
+
+
 
 class MainTemplate extends StatelessWidget {
 
-  Widget Body = Container(), Name = Container();
+  Widget Body = Container();
+  String Name = "Sample App";
 
-  MainTemplate(Widget body, name) {this.Body = body; this.Name = name;}
-
+  MainTemplate(Widget body, String name) {this.Body = body; this.Name = name;}
 
   @override
   Widget build (BuildContext ctx) {
     return Scaffold(
-      appBar: 
-        AppBar(
+      drawer: myDrawer,
+      appBar: AppBar(
+                backgroundColor: Colors.white,
           leading: Padding(
-            padding: EdgeInsets.only(left: 8),
-            child:Image.asset('assets/3toch.png')
-          ),
-          title: Name
-        ),
-        body:  Body
-      );
+            
+            padding: EdgeInsets.only(left: 10),
+            child: Builder(
+                builder: (BuildContext context) {
+          return Align( alignment: Alignment.centerLeft,
+              child:IconButton(
+              onPressed: (){
+                
+                Scaffold.of(context).openDrawer();
+              },
+              icon:  Image(image: AssetImage("assets/3line.png"))
+              )
+              );
+             }
+            ),
+            ),
+
+        title: Row(
+              children:[
+            Image(image: AssetImage("assets/sber.png"))
+          ]),
+          actions: [
+           Align(alignment: Alignment.centerRight,
+          child:Text(Name,textAlign: TextAlign.right, style: TextStyle(color: Color(0xff5b5b5b), fontSize: 18, fontFamily: "SF Pro Display", fontWeight: FontWeight.w500, ), )),
+          Container(width: 32,)
+          ],
+        
+      ),
+      body:  Body
+    );
   }
 }
